a659408e6420ca345294b375fead857eb71bc532
StartFromThird <42039978+StartFromThird@users.noreply.github.com>
2020-01-10 22:44:49 +0800

    实现定时从外部链接更新wx数据库
    1、获取wx数据库全部数据
    2、arr数据比较

--- imusic/cloudfunctions/getPlaylist/index.js
+++ imusic/cloudfunctions/getPlaylist/index.js
@@ -2,10 +2,12 @@
 const cloud = require('wx-server-sdk')
 const rp = require('request-promise');
 const URL = 'http://musicapi.xiecheng.live/personalized'
+const MAX_LIMIT = 100
 cloud.init()
 const db = cloud.database()
 const playlistCollection = db.collection('playlist')
 
+// 单个歌单存入
 const addPlaylist = async (d) => {
   await playlistCollection.add({
     data: {
@@ -18,15 +20,59 @@ const addPlaylist = async (d) => {
     console.error('插入失败')
   })
 }
+// 歌单数组存入
+const addPlaylistArr = async (arr) => {
+  for (let i = 0, len = arr.length; i < len; i++) {
+    await addPlaylist(arr[i])
+  }
+}
+// 获取wx数据库所有列表数据
+const getAllCollectionList = async (collectionName) => {
+  const collection = db.collection(collectionName)
+  const batchTimes = await collection.count().then((res) => {
+    return Math.ceil(res.total / MAX_LIMIT)
+  }).catch((err) => {
+    return 0
+  })
+  // 承载所有读操作的 promise 的数组
+  const tasks = []
+  for (let i = 0; i < batchTimes; i++) {
+    const promise = collection.skip(i * MAX_LIMIT).limit(MAX_LIMIT).get()
+    tasks.push(promise)
+  }
+  // 等待所有
+  return (await Promise.all(tasks)).reduce((acc, cur) => {
+    return {
+      data: acc.data.concat(cur.data),
+      errMsg: acc.errMsg,
+    }
+  })
+}
+// 比较新n旧o数组，依据属性attrN选出新加数据
+const getNewData = (o, n, attrN) => {
+  let oAttrNArr = []
+  let newData = []
+  o.forEach(ele => {
+    if (ele[attrN]) {
+      oAttrNArr.push(ele[attrN])
+    }
+  })
+  n.forEach(ele => {
+    if (ele[attrN] && (oAttrNArr.indexOf(ele[attrN]) == -1)) {
+      newData.push(ele)
+    }
+  })
+  return newData
+}
 // 云函数入口函数
 exports.main = async (event, context) => {
-  const playlist = await rp(URL).then((res) => {
+  // wx数据库所有数据
+  const playlistCol_wx = (await getAllCollectionList('playlist')).data
+  // 外部接口数据
+  const playlistCol_url = await rp(URL).then((res) => {
     return JSON.parse(res).result
   });
-  // playlist.map((ele) => {
-  //   await addPlaylist(ele)
-  // });
-  for (let i = 0, len = playlist.length; i < len; i++) {
-    await addPlaylist(playlist[i])
-  }
+  // 存入外部接口新增数据
+  const newPlaylist = getNewData(playlistCol_wx, playlistCol_url, 'id')
+  await addPlaylistArr(newPlaylist)
 }
